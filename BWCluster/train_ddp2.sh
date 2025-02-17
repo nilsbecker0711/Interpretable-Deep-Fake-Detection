d42b91bd66e5d1b1b4a2cf37408671515fd2a362
nilsbecker0711 <nils.becker0711@gmail.com>
2025-02-17 17:08:50 +0100

    #4 new train sh

--- BWCluster/train_ddp2.sh
+++ BWCluster/train_ddp2.sh
@@ -19,10 +19,10 @@ echo "Environment: $(conda info --envs)"
 module load devel/cuda/12.4
 
 export RANK=0                  # Set the rank of the current process (0 for first process)
-export WORLD_SIZE=4            # Set the total number of processes (2 for two GPUs)
+export WORLD_SIZE=2            # Set the total number of processes (2 for two GPUs)
 # export LOCAL_RANK=0            # Local rank (used by DDP for each process)
 export MASTER_ADDR="localhost"  # The master node's address (typically localhost for single-node)
 export MASTER_PORT=29300    # The port for communication (can be any available port)
 
 # Launch the training with two GPUs
-torchrun --nproc_per_node=4 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=29300 ~/Interpretable-Deep-Fake-Detection/training/train.py --detector_path ~/Interpretable-Deep-Fake-Detection/training/config/detector/xception.yaml --ddp
+torchrun --nproc_per_node=4 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=29300 ~/Interpretable-Deep-Fake-Detection/training/train.py --detector_path ~/Interpretable-Deep-Fake-Detection/training/config/detector/vgg_bcos.yaml --ddp
