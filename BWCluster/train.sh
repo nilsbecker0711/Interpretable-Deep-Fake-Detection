a5f9469952b33b69d65ca004906d77bf8457e1bd
kai.reffert <kai.reffert@students.uni-mannheim.de>
2025-02-17 17:19:14 +0100

    #8 andb, and notebooks

--- BWCluster/train.sh
+++ BWCluster/train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p gpu_4  # Use the dev_gpu_4_a100 partition with A100 GPUs
+#SBATCH -p dev_gpu_4  # Use the dev_gpu_4_a100 partition with A100 GPUs
 #SBATCH -n 1                   # Number of tasks (1 for single node)
-#SBATCH -t 03:00:00            # Time limit (10 minutes for debugging purposes)
-#SBATCH --mem=60000             # Memory request (adjust as needed)
+#SBATCH -t 00:20:00            # Time limit (10 minutes for debugging purposes)
+#SBATCH --mem=30000             # Memory request (adjust as needed)
 #SBATCH --gres=gpu:1           # Request 1 GPU (adjust if you need more)
 #SBATCH --cpus-per-task=16     # Number of CPUs per GPU (16 for A100)
 #SBATCH --ntasks-per-node=1    # Number of tasks per node (1 in this case)
