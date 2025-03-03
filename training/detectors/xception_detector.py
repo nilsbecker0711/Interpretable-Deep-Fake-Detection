d94270b6c5abf252809f2f6d75e4482a27632ea4
TiloSchuetz <schuetz.tilo@gmail.com>
2025-03-03 16:48:39 +0100

    #25 include f1 and rc in detectors

--- training/detectors/xception_detector.py
+++ training/detectors/xception_detector.py
@@ -99,8 +99,8 @@ class XceptionDetector(AbstractDetector):
         label = data_dict['label']
         pred = pred_dict['cls']
         # compute metrics for batch data
-        auc, eer, acc, ap = calculate_metrics_for_train(label.detach(), pred.detach())
-        metric_batch_dict = {'acc': acc, 'auc': auc, 'eer': eer, 'ap': ap}
+        auc, eer, acc, ap, rc, f1 = calculate_metrics_for_train(label.detach(), pred.detach())
+        metric_batch_dict = {'acc': acc, 'auc': auc, 'eer': eer, 'ap': ap, 'rc': rc, 'f1': f1}
         # we dont compute the video-level metrics for training
         self.video_names = []
         return metric_batch_dict
