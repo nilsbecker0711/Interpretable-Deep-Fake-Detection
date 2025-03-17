1ee1526e4d453e768c8f527f07c91786e4421229
tubbers7 <toby.fuchs@me.com>
2025-03-17 17:54:40 +0100

    metrics class angepasst

--- training/metrics/base_metrics_class.py
+++ training/metrics/base_metrics_class.py
@@ -53,7 +53,7 @@ def calculate_metrics_for_train(label, output):
                                                  pos_label=1)
     except:
         # for the case when we only have one sample
-        return None, None, accuracy, ap
+        return None, None, accuracy, ap, None, None
 
     if np.isnan(fpr[0]) or np.isnan(tpr[0]):
         # for the case when all the samples within a batch is fake/real
