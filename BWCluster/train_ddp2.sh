8dbaa53015f571e1ef46214dbfd25ead19e5cdbc
kai.reffert <kai.reffert@students.uni-mannheim.de>
2025-03-24 09:53:18 +0100

    notebook

--- BWCluster/train_ddp2.sh
+++ BWCluster/train_ddp2.sh
@@ -2,7 +2,7 @@
 #SBATCH -p gpu_8  # Use the dev_gpu_4_a100 partition with A100 GPUs
 #SBATCH -n 1                   # Number of tasks (1 for single node)
 #SBATCH -t 02:00:00            # Time limit (10 minutes for debugging purposes)
-#SBATCH --mem=30000             # Memory request (adjust as needed)
+#SBATCH --mem=60000             # Memory request (adjust as needed)
 #SBATCH --gres=gpu:2          # Request 1 GPU (adjust if you need more)
 #SBATCH --cpus-per-task=16     # Number of CPUs per GPU (16 for A100)
 #SBATCH --ntasks-per-node=1    # Number of tasks per node (1 in this case)
@@ -25,4 +25,4 @@ export MASTER_ADDR="localhost"  # The master node's address (typically localhost
 export MASTER_PORT=28700    # The port for communication (can be any available port)
 
 # Launch the training with two GPUs
-torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=28700 ~/Interpretable-Deep-Fake-Detection/training/train.py --detector_path ~/Interpretable-Deep-Fake-Detection/training/config/detector/resnet34_bcos_v2.yaml --ddp
+torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=28700 ~/Interpretable-Deep-Fake-Detection/training/train.py --detector_path ~/Interpretable-Deep-Fake-Detection/training/config/detector/resnet34_bcos_v2_minimal.yaml --ddp
