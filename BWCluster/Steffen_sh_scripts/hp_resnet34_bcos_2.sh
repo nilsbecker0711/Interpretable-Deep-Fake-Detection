227456b45ba49baf64d057bf8b2155933eb22834
Steffen Jung <33086982+steffen-jung@users.noreply.github.com>
2025-04-28 19:08:33 +0200

    modified paths

--- BWCluster/Steffen_sh_scripts/hp_resnet34_bcos_2.sh
+++ BWCluster/Steffen_sh_scripts/hp_resnet34_bcos_2.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#SBATCH -p dev_gpu_a100_il  # Use the dev_gpu_4_a100 partition with A100 GPUs dev_gpu_4
+#SBATCH -p gpu20  # Use the dev_gpu_4_a100 partition with A100 GPUs dev_gpu_4
 #SBATCH -n 1                   # Number of tasks (1 for single node)
-#SBATCH -t 20           # Time limit (10 minutes for debugging purposes)
-#SBATCH --mem=50000             # Memory request (adjust as needed)
+#SBATCH -t 23:59:00           # Time limit (10 minutes for debugging purposes)
 #SBATCH --gres=gpu:2           # Request 1 GPU (adjust if you need more)
-#SBATCH --cpus-per-task=16     # Number of CPUs per GPU (16 for A100)
 #SBATCH --ntasks-per-node=1    # Number of tasks per node (1 in this case)
+#SBATCH --cpus-per-task=16     # Number of CPUs per GPU (16 for A100)
 
-module load devel/miniforge
+trap "trap ' ' TERM INT; kill -TERM 0; wait" TERM INT
 
-conda activate /pfs/work7/workspace/scratch/ma_tischuet-team_project_explainable_deepfakes/envs/TP_main_2
+eval "$(conda shell.bash hook)"
+
+conda activate /BS/robust-architectures/work/bcos-df-env
+cd /BS/robust-architectures/work/bcos-df
 
 echo "Running on $(hostname)"
 echo "Date: $(date)"
 echo "Python version: $(python --version)"
 echo "Environment: $(conda info --envs)"
-
-module load devel/cuda/12.8
 
 # python ~/Interpretable-Deep-Fake-Detection/training/hp_tuning.py --detector_path ~/Interpretable-Deep-Fake-Detection/training/config/detector/resnet34_bcos.yaml
 
@@ -27,5 +27,5 @@ export MASTER_ADDR="localhost"  # The master node's address (typically localhost
 export MASTER_PORT=29100    # The port for communication (can be any available port)
 
 # Launch the training with two G<PUs
-torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=29100 ~/Interpretable-Deep-Fake-Detection/training/hp_tuning.py --detector_path ~/Interpretable-Deep-Fake-Detection/training/config/detector/resnet34_bcos_v2.yaml --ddp --sweep_id 9v62b1zw
+torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=29100 /BS/robust-architectures/work/bcos-df/training/hp_tuning.py --detector_path /BS/robust-architectures/work/bcos-df/training/config/detector/resnet34_bcos_v2.yaml --ddp --sweep_id 9v62b1zw
 #python ~/Interpretable-Deep-Fake-Detection/training/hp_tuning.py --detector_path ~/Interpretable-Deep-Fake-Detection/training/config/detector/resnet34_bcos_v2.yaml --sweep_id 
